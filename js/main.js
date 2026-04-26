8ad06b04905a4b4657d5af678b6d82c199ce673d
Mikailov05 <ahmed.mikailov@interner.ru>
2026-04-26 23:58:45 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("\n=== Деструктуризация массивов ===");
+/*console.log("\n=== Деструктуризация массивов ===");
 
 const colors = ["red", "green", "blue"];
 
@@ -13,7 +13,7 @@ const [primary, , tertiary] = colors;
 console.log(primary, tertiary);
 
 const [c1, c2, c3, c4 = "yellow"] = colors;
-console.log(c4); // yellow
+console.log(c4);
 
 console.log("\n=== Деструктуризация объектов ===");
 
@@ -61,7 +61,6 @@ function printUser({ name, age, city }) {
 }
 
 // 
-
 // 
 // 
 // 
@@ -86,3 +85,71 @@ function printUser({ name, age, city }) {
 // 
 // // 
 // printProduct(product);
+
+// console.log("Spread для массивов");
+// 
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// 
+// const combined = [...arr1, ...arr2];
+// console.log("Объединённый массив:", combined);
+// 
+// const copy = [...arr1];
+// console.log("Копия массива:", copy);
+// 
+// const extended = [0, ...arr1, 7, 8];
+// console.log("Расширенный массив:", extended);
+// 
+// console.log("Spread для объектов");
+// 
+// const person = {
+//   name: "Иван",
+//   age: 30,
+// };
+// 
+// const address = {
+//   city: "Санкт-Петербург",
+//   street: "Невский проспект",
+// };
+// 
+// const fullInfo = { ...person, ...address };
+// console.log("Полная информация:", fullInfo);
+// 
+// const personCopy = { ...person };
+// console.log("Копия объекта:", personCopy);
+// 
+// const updated = { ...person, age: 31, occupation: "Developer" };
+// console.log("Обновлённый объект:", updated);
+// 
+// console.log("Rest оператор");
+// 
+// function sum(...numbers) {
+//   return numbers.reduce((total, num) => total + num, 0);
+// }
+// 
+// console.log("Сумма 1,2,3:", sum(1, 2, 3));
+// console.log("Сумма 1,2,3,4,5:", sum(1, 2, 3, 4, 5));
+// 
+// const numbers = [10, 20, 30, 40, 50];
+// const [first, second, ...rest] = numbers;
+// console.log("Первое число:", first);
+// console.log("Второе число:", second);
+// console.log("Остальные числа:", rest);
+
+*/
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const combinedArr = [...arr1, ...arr2];
+console.log("Объединённый массив:", combinedArr);
+
+function findMax(...numbers) {
+    return Math.max(...numbers);
+}
+console.log(findMax(10, 20, 30, 40, 50));
+
+const objA = { a: 1, b: 2 };
+const objB = { c: 3, d: 4 };
+const merged = {...objA, ...objB };
+console.log("Объединённый объект:", merged);
