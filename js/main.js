d758ecfaa55b95ee04f163216fcd3f3e6bfe6d51
Mikailov05 <ahmed.mikailov@interner.ru>
2026-04-27 00:08:54 +0300

    promises in JS

--- js/main.js
+++ js/main.js
@@ -167,3 +167,97 @@ console.log("4 * 7 =", умножить(4, 7));
 import * as Utils from "./utils.js";
 console.log(Utils.greet("Мария"));
 console.log("Умножение:", Utils.multiply(3, 9));*/
+
+/*
+
+console.log("Промисы");
+
+const simplePromise = new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+        resolve("Операция выполнена успешно!");
+    } else {
+        reject("Произошла ошибка!");
+    }
+});
+
+simplePromise
+    .then((result) => console.log("Результат:", result))
+    .catch((error) => console.log("Ошибка:", error));
+
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Прошло ${ms} миллисекунд`);
+        }, ms);
+    });
+}
+
+delay(1000)
+    .then((message) => console.log(message));
+
+function fetchUserData(userId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (userId > 0) {
+                resolve({
+                    id: userId,
+                    name: "Иван Иванов",
+                    email: "ivan@example.com",
+                });
+            } else {
+                reject("Неверный ID пользователя");
+            }
+        }, 1500);
+    });
+}
+
+fetchUserData(1)
+    .then((user) => console.log("Пользователь:", user))
+    .catch((error) => console.log("Ошибка:", error));
+
+function step1() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve("Шаг 1 завершён"), 500);
+    });
+}
+
+function step2(predsResult) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(`${predsResult} -> Шаг 2 завершён`), 500);
+    });
+}
+
+function step3(predsResult) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(`${predsResult} -> Шаг 3 завершён`), 500);
+    });
+}
+
+step1()
+    .then((result1) => step2(result1))
+    .then((result2) => step3(result2))
+    .then((finalResult) => console.log("Финальный результат:", finalResult))
+    .catch((error) => console.log("Ошибка в цепочке:", error));
+    */
+
+
+
+
+function checkInventory(productName, inStock) {
+    return new Promise((resolve, reject) => {
+        if (inStock) {
+            resolve(`Товар "${productName}" есть в наличии`);
+        } else {
+            reject(`Ошибка: товара "${productName}" нет на складе`);
+        }
+    });
+}
+
+checkInventory("Ноутбук", true)
+    .then((message) => console.log(message))
+    .catch((error) => console.log(error));
+
+checkInventory("Телефон", false)
+    .then((message) => console.log(message))
+    .catch((error) => console.log(error));
